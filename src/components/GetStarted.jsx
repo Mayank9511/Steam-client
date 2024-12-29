91dff8ed802b79c2cac35e6b791766a66eb09ec3
Mayank9511 <gupta.mayank.mg01@gmail.com>
2024-12-29 15:23:05 +0530

    updated server url

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -14,7 +14,7 @@ const GetStarted = () => {
 
       try {
         const res = await axios.post(
-          "http://localhost:5000/api/text",
+          "https://steam-server.onrender.com/api/text",
           { question }, // Send question as JSON
           {
             headers: {
